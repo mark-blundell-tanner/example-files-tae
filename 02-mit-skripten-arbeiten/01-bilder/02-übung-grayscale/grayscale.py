5c95b9ca6f05e7064b7ca7d87267d0d5f4ace9b4
mark-blundell-tanner <mark.blundell@tanner.de>
2026-03-03 05:27:14 +0000

    fix: dev container and script notes

--- 02-mit-skripten-arbeiten/01-bilder/02-übung-grayscale/grayscale.py
+++ 02-mit-skripten-arbeiten/01-bilder/02-übung-grayscale/grayscale.py
@@ -1,1 +1,2 @@
-# Generieren Sie einen Python-Skript, das ein Bild in Graustufen umwandelt.
+# Generieren Sie einen Python-Skript, der ein Bild in Graustufen umwandelt.
+# Denken Sie daran, dass der Skript vom Root-Ordner aus ausgeführt wird
---
+++ 02-mit-skripten-arbeiten/02-pdf/02-übung-pdfs-zusammenführen/pdf-zusammenführen.py
@@ -0,0 +1,2 @@
+# Generieren Sie einen Python-Skript, der die PDFs im Ordner zusammenführt
+# Denken Sie daran, dass der Skript vom Root-Ordner aus ausgeführt wird
